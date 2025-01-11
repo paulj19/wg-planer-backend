c20c49ea982c562595ace68f7f4f5d2415f89004
paul <diljosepaul@gmail.com>
2025-01-11 10:02:40 +0100

    update db name

--- db.go
+++ db.go
@@ -17,7 +17,8 @@ import (
 var collection *mongo.Collection
 var client *mongo.Client
 // var DB_URI = "mongodb://localhost:27018"
-var DB_URI = os.Getenv("MONGO_URL")
+// var DB_URI = os.Getenv("MONGO_URL")
+var DB_URI = "mongodb://wg-planer:1a865dab20bcbdd2a6015d4b81bb594d@172.17.0.6:27017/wg_planer"
 
 func initMongo(ctx context.Context) {
 // 	credential := options.Credential{
